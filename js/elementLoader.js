b89cbe8fd5e5460edbd771f711ccd47be6f76f3d
Egor Shumeyko <tierdal@hotmail.com>
2021-02-09 00:24:49 -0600

    another one

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -11,11 +11,10 @@ $(function () {
     $("#navbar-load").load("/uh_projects_pbconsulting/page_elements/navbar.html");
 });
 
-//https://html-online.com/articles/highlight-active-menu-item-script/
-$(function(){
-    $("a").each(function(){
-            if ($(this).attr("href") == window.location.pathname){
-                    $(this).addClass("menu_active");
-            }
+$(document).ready(function() {
+    $('a').each(function() {
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('menu_active');
+        }
     });
 });
