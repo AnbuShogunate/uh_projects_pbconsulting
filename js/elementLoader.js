1dca1d334df13163e19d4af0c242cb035c8d31e3
Egor Shumeyko <tierdal@hotmail.com>
2021-02-08 23:24:07 -0600

    small update to js

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -15,7 +15,7 @@ $(function() {
     var url = window.location.href;
 
     // passes on every "a" tag
-    $(".navBar a").each(function() {
+    $("#navBar a").each(function() {
         // checks if its the same on the address bar
         if (url == (this.href)) {
             $(this).closest("li").addClass("menu_active");
