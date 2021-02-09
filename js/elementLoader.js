a949a767d2582279b65ff15d7c1ef1814e222f6b
Egor Shumeyko <tierdal@hotmail.com>
2021-02-08 19:06:41 -0600

    modify js

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -1,3 +1,6 @@
 function load_home() {
     document.getElementById("header-loader").innerHTML='<object type="text/html" data="header.html" ></object>';
 }
+$(function () {
+    $("#header-loader").load("/uh_projects_pbconsulting/page_elements/header.html");
+});
