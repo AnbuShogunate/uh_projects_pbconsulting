31930ab3ebcdead7a13841cccf69081779674eb9
Egor Shumeyko <tierdal@hotmail.com>
2021-02-08 23:21:11 -0600

    update nav bar and add js for highlighting

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -10,3 +10,17 @@ $(function () {
 $(function () {
     $("#navbar-load").load("/uh_projects_pbconsulting/page_elements/navbar.html");
 });
+$(function() {
+    // this will get the full URL at the address bar
+    var url = window.location.href;
+
+    // passes on every "a" tag
+    $(".navBar a").each(function() {
+        // checks if its the same on the address bar
+        if (url == (this.href)) {
+            $(this).closest("li").addClass("menu_active");
+            //for making parent of submenu active
+           $(this).closest("li").parent().parent().addClass("menu_active");
+        }
+    });
+});  
