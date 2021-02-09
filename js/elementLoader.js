a99af9f02585c07bc537484963345e4b94d36f40
Egor Shumeyko <tierdal@hotmail.com>
2021-02-08 19:14:03 -0600

    it works

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -1,3 +1,6 @@
 $(function () {
     $("#header-load").load("/uh_projects_pbconsulting/page_elements/header.html");
 });
+$(function () {
+    $("#hfooter-load").load("/uh_projects_pbconsulting/page_elements/footer.html");
+});
