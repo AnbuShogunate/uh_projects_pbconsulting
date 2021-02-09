1389456817ca06088ef9583c5ce3a80661dee92a
Egor Shumeyko <tierdal@hotmail.com>
2021-02-09 00:22:10 -0600

    trying something new

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -12,12 +12,10 @@ $(function () {
 });
 
 //https://html-online.com/articles/highlight-active-menu-item-script/
-$(document).ready(function() {
-    console.log($(this).attr('href'));
-    $("#navbar-load a").each(function() {
-        if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
-            $(this).parent().addClass('menu_active');
-        }
-        console.log($(this).attr('href'));
+$(function(){
+    $(".main_menu a").each(function(){
+            if ($(this).attr("href") == window.location.pathname){
+                    $(this).addClass("menu_active");
+            }
     });
 });
