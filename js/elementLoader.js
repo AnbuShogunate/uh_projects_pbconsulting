49bda886f4fddd66da2b2985a68622550d791329
Egor Shumeyko <tierdal@hotmail.com>
2021-02-09 00:13:16 -0600

    push some debug

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -11,39 +11,12 @@ $(function () {
     $("#navbar-load").load("/uh_projects_pbconsulting/page_elements/navbar.html");
 });
 
-$("#navBar li a").click(function() {
-    var $Link = $(this); // cache it as we will use ot mote than once
-    //also url == a.href cannot return true if you use relative url in the link.
-    //url most likely http://domain.com/pagename href will be just a page name
-    //if active do nothing
-    if (!$Link.hasClass("menu_active")) {
-        $Link.closest("ul") //find menu container
-            .find("li.menu_active").removeClass('menu_active'); //find active and remove it
-        $Link.addClass('menu_active');
-    }
-});
-
-
-/* //https://makitweb.com/highlight-current-page-menu-item-with-jquery/
+//https://html-online.com/articles/highlight-active-menu-item-script/
 $(document).ready(function() {
-    var url = window.location.href;
-    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
-    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
-    url = url.substr(url.lastIndexOf("/") + 1);
-    if(url == ''){
-        url = 'index.html';
-    }
-    $('.main_menu_ul li').each(function(){
-        var href = $(this).find('a').attr('href');
-        if(url == href){
-            var parentClass = $(this).parent('ul').attr('class');
-            if(parentClass == 'submenu'){
-                $(this).addClass('submenu_active');
-                $(this).parents('.main_menu_ul li').addClass('menu_active');
-            }else{
-                $(this).addClass('menu_active');
-            }
-
+    console.log($(this).attr('href'));
+    $(".main_menu_ul a").each(function() {
+        if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+            $(this).parent().addClass('menu_active');
         }
     });
-}); */
+});
