66aba5e5b56697d53f858d83b32d795e06386a29
Egor Shumeyko <tierdal@hotmail.com>
2021-02-08 19:01:01 -0600

    maybe this will work

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -1,3 +1,3 @@
 function load_home() {
-    document.getElementById("header-loader").innerHTML='<object type="text/html" data="/uh_projects_pbconsulting/page_elements/header.html" ></object>';
+    document.getElementById("header-loader").innerHTML='<object type="text/html" data="header.html" ></object>';
 }
