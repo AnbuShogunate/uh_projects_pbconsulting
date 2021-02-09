f6b499adea1a990f270ea79105f1e5013325c049
Egor Shumeyko <tierdal@hotmail.com>
2021-02-08 23:42:51 -0600

    testing more js

--- js/elementLoader.js
+++ js/elementLoader.js
@@ -10,10 +10,27 @@ $(function () {
 $(function () {
     $("#navbar-load").load("/uh_projects_pbconsulting/page_elements/navbar.html");
 });
-$(function(){
-    $('.main_menu_ul li a').each(function(){
-        if (curURL.indexOf(this.href) != -1){
-            $(this).closest('li').addClass('menu_active');
+
+//https://makitweb.com/highlight-current-page-menu-item-with-jquery/
+$(document).ready(function() {
+    var url = window.location.href;
+    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+    url = url.substr(url.lastIndexOf("/") + 1);
+    if(url == ''){
+        url = 'index.html';
+    }
+    $('.main_menu_ul li').each(function(){
+        var href = $(this).find('a').attr('href');
+        if(url == href){
+            var parentClass = $(this).parent('ul').attr('class');
+            if(parentClass == 'submenu'){
+                $(this).addClass('menu_active');
+                //$(this).parents('.main_menu_ul li').addClass('menu_active');
+            }else{
+                $(this).addClass('menu_active');
+            }
+
         }
     });
 });
