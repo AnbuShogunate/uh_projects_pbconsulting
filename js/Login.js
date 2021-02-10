5c904501041d6b63434576773478145a07bc5745
Weizhao Chen <wchen29@uh.edu>
2021-02-10 17:37:33 -0600

    jstest1
    test1

--- js/Login.js
+++ js/Login.js
@@ -1,6 +1,7 @@
 //var loginsystem = document.getElementById("login_system");
 var loginsubmit = document.getElementById("login_button");
 var loginsystem = document.getElementsByClassName("login_system");
+var logintable = document.getElementById("login_table");
 
 /*
 
@@ -16,13 +17,37 @@ var loginsystem = document.getElementsByClassName("login_system");
         //window.location.href = 'CustomerMainPage.html';
     }
 })
-*/
+
 
 function login(loginsubmit) {
     if (loginsubmit.getElementById("username_id") == "Admin" && loginsubmit.getElementById("password_id") == "root") {
-        location = '/uh_projects_pbconsulting/helpdesk/index.html';
+        location = 'C:\Users\ManaMenu\Documents\GitHub\uh_projects_pbconsulting\helpdesk\index.html';
     }
     else {
         alert("invalid info");
     }
 }
+*/
+
+function login() {
+    var username = document.getElementById("username_id");
+    var password = document.getElementById("password_id");
+
+    var username_input = username.value;
+    var password_input = password.value;
+
+    if (username_input == "admin" && password_input == "root") {
+        //CookieSave(username_input);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+/*
+function CookieSave(value) {
+    var expire = new Date();
+
+
+}
+*/
