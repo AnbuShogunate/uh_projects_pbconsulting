05e006f7c8b5d1a360b3e73f94151c9b20de6c6d
Weizhao Chen <wchen29@uh.edu>
2021-02-09 18:05:54 -0600

    logintest
    test4

--- js/Login.js
+++ js/Login.js
@@ -5,15 +5,14 @@ var loginsubmit = document.getElementById("login_button");
 
 loginsubmit.addEventListener("click", (e) => {
     e.preventDefault();
-    const name = loginsystem.user_username.value;
-    const pass = loginsystem.user_password.value;
+    var name = loginsystem.user_username.value;
+    var pass = loginsystem.user_password.value;
 
     if (name === "root" && pass === "admin") {
-        alert("oh yeah");
-        window.location.href = '/uh_projects_pbconsulting/helpdesk/index.html';
+        //window.location.href = '/uh_projects_pbconsulting/helpdesk/index.html';
     }
     else if (name === "customer" && pass === "sample") {
-        alert("welcome back!");
-        window.location.href = 'CustomerMainPage.html';
+        //window.location.href = 'CustomerMainPage.html';
     }
 })
+
