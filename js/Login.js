f9a2658f8af669135b97fc0d44fc70bcd79fcf8b
Weizhao Chen <wchen29@uh.edu>
2021-02-11 11:46:04 -0600

    Update Login.js
    not ideal, just ganna force it atm and then make the logout button invisible when not logged in

--- js/Login.js
+++ js/Login.js
@@ -31,6 +31,7 @@ function validation(){
 }
 
 function unvalidate(){
+    checklogin = true;
     if(checklogin === true)
     {
         document.cookie = 'username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/'; //removes cookie
