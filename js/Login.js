e0fa53359aba8bb83a61782ffd3ab3e8ae24f37e
Weizhao Chen <wchen29@uh.edu>
2021-02-11 02:27:02 -0600

    stuff

--- js/Login.js
+++ js/Login.js
@@ -1,34 +1,3 @@
-//var loginsystem = document.getElementById("login_system");
-//var loginsubmit = document.getElementById("login_button");
-//var loginsystem = document.getElementsByClassName("login_system");
-//var logintable = document.getElementById("login_table");
-
-/*
-
-//loginsubmit.addEventListener("click", (e) => {
-    e.preventDefault();
-    var name = loginsystem.user_username.value;
-    var pass = loginsystem.user_password.value;
-
-    if (name === "root" && pass === "admin") {
-        //window.location.href = '/uh_projects_pbconsulting/helpdesk/index.html';
-    }
-    else if (name === "customer" && pass === "sample") {
-        //window.location.href = 'CustomerMainPage.html';
-    }
-})
-
-
-function login(loginsubmit) {
-    if (loginsubmit.getElementById("username_id") == "Admin" && loginsubmit.getElementById("password_id") == "root") {
-        location = 'C:\Users\ManaMenu\Documents\GitHub\uh_projects_pbconsulting\helpdesk\index.html';
-    }
-    else {
-        alert("invalid info");
-    }
-}
-*/
-
 var username = document.getElementById("username_id").value;
 var password = document.getElementById("password_id").value;
 
