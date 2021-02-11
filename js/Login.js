7cfb62dde84d56f74d975e56a83df88b8459d774
Weizhao Chen <wchen29@uh.edu>
2021-02-11 01:50:41 -0600

    Update Login.js

--- js/Login.js
+++ js/Login.js
@@ -35,19 +35,6 @@ var password = document.getElementById("password_id");
 var username_input = username.value;
 var password_input = password.value;
 
-var checker = 
-
-function login(){
-    something();
- //   if(getCookie(username_input)=="admin"){
-  //      alert("you are already logged in!");
- //       window.location = "/uh_projects_pbconsulting/helpdesk/index.html";
- //   }
- //   else{
-        
-   // }
-}
-
 function validation(){
     if (username_input === "admin" && password_input === "root") {
         alert("welcome back!");
@@ -55,56 +42,5 @@ function validation(){
     }
     else {
         alert("invalid username or password!");
-    }
-}
-
-function leCookie() {
-}
-function logout(){
-    document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
-    //setcookie("username", username_input, 0);
-    document.cookie = username_input + '=;expires=Thu, 01 Jan 2000 00:00:01 GMT;';
-    window.location = "/uh_projects_pbconsulting/index.html";
-}
-
-
-
-function cookiecheck() {
-    if (cookie == 1) {
-        window.location = "/uh_projects_pbconsulting/helpdesk/index.html";
-    }
-    else {
-        validation();
-    }
-}
-
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function checkCookie() {
-    var username = getCookie("username");
-    if (username != "") {
-        alert("You are already logged in!");
-    }
-    else {
-        alert("please login first!");
-        window.location = "/uh_projects_pbconsulting/index.html"; 
     }
 }
